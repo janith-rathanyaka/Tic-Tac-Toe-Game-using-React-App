2f56fc32ffbf63e6a6ce00e5349342043b23e19b
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2020-12-25 21:43:10 +0530

    update details

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 export default class Game extends React.Component{
-     return ( 
-     <button className="square" onClick={props.onClick}>
-     {props.value}
-     </button>
- );
+     constructor(props){
+         super(props);
+         this.state ={
+            xIsNext: true,
+            stepNumber: 0,
+            history:[
+                {squares: Array(9).fill(null)}
+            ],
+
+         };
+        
+
+     }
     
+     jumpTo(step){
+        this.setState({
+            stepNumber:step,
+            xIsNext:step % 2 === 0, 
+        }); 
+     }
 }
